f01279857e0ea92489597def95913202e12cc326
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-05-22 11:53:25 +0100

    Learnt more on input and props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Google from './components/Google'
 import ReactHooks from './components/ReactHooks'
 import Mapping from './components/Mapping'
 import Signin from './components/Signin'
+import Input from './components/Input'
 const App = () => {
   return (
     <div>
       <ReactHooks/>
       <Mapping/>
-      <Signin/>
+      {/* <Signin/> */}
+
+      <Input/>
     </div>
   )
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+const Input = () => {
+
+    const [firstname,setfirstname] = useState('')
+    const [lastname, setlastname] = useState('')
+    const [username, setusername] = useState('')
+    const [password, setpassword] = useState('')
+    const [user, setUser] = useState([])
+   
+    const saveUser = ()=>{
+        console.log(firstname,lastname);
+        setUser([...user, {firstname,lastname,username,password}])
+        console.log(user);
+    }
+
+  return (
+    <div className='text-center max-w-md mx-auto p-4'>
+        <input 
+            type="text" 
+            className='border-2 border-green-300 rounded  block mb-3 w-full' 
+            onChange={(e)=>setfirstname(e.target.value)}
+            placeholder='First Name' />
+        <input 
+            type="text" 
+            className='border-2 border-green-300 rounded  block mb-3 w-full' 
+            onChange={(e)=>setlastname(e.target.value)}
+            placeholder='Last Name' />
+        <input 
+            type="text" 
+            className='border-2 border-green-300 rounded  block mb-3 w-full' 
+            onChange={(e)=>setusername(e.target.value)}
+            placeholder='User Name' />
+        <input 
+            type="text" 
+            className='border-2 border-green-300 rounded  block mb-3 w-full' 
+            onChange={(e)=>setpassword(e.target.value)}
+            placeholder='Password' />
+        <button 
+            className='border-2 bg-green-300 p-2 rounded hover:bg-transparent hover:text-black hover:border-green-300 text-white w-full transition duration-300'
+            onClick={saveUser}
+        >Change name</button>
+
+        <div>
+            {user.map((res, index)=>(
+                <div className='flex justify-between my-3 bg-violet-300 py-2 px-3 rounded' key={index}>
+                    <p>{res.firstname}</p>
+                    <p>{res.lastname}</p>
+                    <p>{res.username}</p>
+                    <p>{res.password}</p>
+                </div>
+            ))}
+        </div>
+
+    </div>
+  )
+}
+
+export default Input
