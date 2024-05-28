99a803e75fd6b3f7e5a0ccaeb55ed6ce91a1f4bd
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-05-28 10:07:58 +0100

    Added sign in and sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Mapping from './components/Mapping'
 import Signin from './components/Signin'
 import Input from './components/Input'
 import Api from './components/Api'
+import Signup from './components/Signup'
 const App = () => {
   return (
     <div>
       <ReactHooks/>
       <Mapping/>
-      {/* <Signin/> */}
+      <Signin/>
+      <Signup/>
       <Input/>
       <Api/>
     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,10 @@
-import { Button, FloatingLabel } from 'flowbite-react'
 import React from 'react'
 
-const Signin = () => {
+const Signup = () => {
   return (
-    <form action='' method='' className='max-w-screen-md mx-auto rounded shadow-lg px-4 py-10 flex flex-col gap-5'>
+    <form action='' method='' className='max-w-screen-md mx-auto rounded shadow-lg px-4 py-10 flex flex-col gap-5 mt-5'>
         <h1 className='text-2xl font-bold text-center'>Sign In</h1>
+        <input className='border-0 border-b-2 border-b-green-500 focus:border-b-black focus:ring-0' placeholder='Enter your full name' type="text" />
         <input className='border-0 border-b-2 border-b-green-500 focus:border-b-black focus:ring-0' placeholder='Enter your email' type="text" />
         <input className='border-0 border-b-2 border-b-green-500 focus:border-b-black focus:ring-0' placeholder='Enter your password' type="text" />
         <button className='bg-green-500 text-white py-2 rounded'>Sign In</button>
@@ -12,4 +12,4 @@ const Signin = () => {
   )
 }
 
-export default Signin
+export default Signup
